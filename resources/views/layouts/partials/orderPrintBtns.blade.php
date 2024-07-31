57bfb0c7f24ddd15500d74531854be95bb4c591e
Ad <thenewadeel@gmail.com>
2024-08-01 00:04:26 +0500

    pdf btn removed from un-saved orders

--- resources/views/layouts/partials/orderPrintBtns.blade.php
+++ resources/views/layouts/partials/orderPrintBtns.blade.php
@@ -1,11 +1,11 @@
 <div class="btn-group ">
-    <a href="" class="btn btn-dark btn-sm p-0 my-0 align-middle disabled ">
-        <i class="fas fa-print text-xs"></i>
-    </a>
-    <a href="{{ route('orders.print', $order) }}" class="btn btn-outline-primary btn-sm py-0 my-0 px-1 align-middle">
-        <span class="text-sm font-bold">Pdf</span>
-    </a>
     @if ($order->POS_number != null)
+        <a href="" class="btn btn-dark btn-sm p-0 my-0 align-middle disabled ">
+            <i class="fas fa-print text-xs"></i>
+        </a>
+        <a href="{{ route('orders.print', $order) }}" class="btn btn-outline-primary btn-sm py-0 my-0 px-1 align-middle">
+            <span class="text-sm font-bold">Pdf</span>
+        </a>
         <a href="{{ route('orders.print.POS', $order) }}" class="btn btn-outline-dark btn-sm py-0 my-0 px-1 align-middle">
             <span class="text-sm font-bold">POS</span>
         </a>

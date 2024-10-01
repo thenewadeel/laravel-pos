fa12f699760b822f68a3b46df92f29b8a8e9ca85
Ad <thenewadeel@gmail.com>
2024-10-01 13:51:56 +0500

    Cashiers report summary improved

--- resources/views/layouts/partials/orderPeek.blade.php
+++ resources/views/layouts/partials/orderPeek.blade.php
@@ -1,31 +1,34 @@
-<div class=" px-0 m-0">
-
-    <div class="flex flex-row justify-between w-full ">
-
-        <div class="flex flex-row justify-evenly">
+@if ($asTable ?? false)
+    <tr>
+        <td>
             <a href="{{ route('orders.show', ['order' => $order->id]) }}">
                 {{ $order->POS_number }}
             </a>
-        </div>
-        <div class="flex flex-row justify-evenly">
-            {{ $order->shop?->name }}
-        </div>
-        {{-- <div class="flex flex-row">
-            Total:
-            {{ config('settings.currency_symbol') }}{{ number_format($order->total(), 0) }}
-        </div>
-        <div class="">
-            Discount:
-            {{ config('settings.currency_symbol') }}{{ number_format($order->discountAmount(), 0) }}
-        </div> --}}
-        <div class="">
-            Net Payable:
-            {{ config('settings.currency_symbol') }}{{ number_format($order->balance(), 0) }}
-        </div>
-        <div class="">
-            Received:
-            {{ config('settings.currency_symbol') }}{{ number_format($order->receivedAmount(), 0) }}
+        </td>
+        <td>{{ $order->shop?->name }}</td>
+        <td class="text-right">{{ config('settings.currency_symbol') }}{{ number_format($order->balance(), 0) }}</td>
+        <td class="text-right">{{ config('settings.currency_symbol') }}{{ number_format($order->receivedAmount(), 0) }}
+        </td>
+    </tr>
+@else
+    <div class=" px-0 m-0">
+        <div class="flex flex-row justify-between w-full ">
+            <div class="flex flex-row justify-evenly">
+                <a href="{{ route('orders.show', ['order' => $order->id]) }}">
+                    {{ $order->POS_number }}
+                </a>
+            </div>
+            <div class="flex flex-row justify-evenly">
+                {{ $order->shop?->name }}
+            </div>
+            <div class="">
+                Net Payable:
+                {{ config('settings.currency_symbol') }}{{ number_format($order->balance(), 0) }}
+            </div>
+            <div class="">
+                Received:
+                {{ config('settings.currency_symbol') }}{{ number_format($order->receivedAmount(), 0) }}
+            </div>
         </div>
     </div>
-
-</div>
+@endif

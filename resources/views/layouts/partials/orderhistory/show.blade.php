ea23165d8e0505a46d7fc030d25893f7a6ae01fc
Ad <thenewadeel@gmail.com>
2024-10-12 02:33:37 +0500

    layout changed

--- resources/views/layouts/partials/orderhistory/show.blade.php
+++ resources/views/layouts/partials/orderhistory/show.blade.php
@@ -1,5 +1,5 @@
-<div class="mt-2">
-    <table class="table table-sm table-striped">
+<div class="mt-2 flex flex-row overflow-y-scroll bg-slate-200 rounded-md p-1 shadow-md hover:bg-blue-200 text-slate-700">
+    {{-- <table class="table table-sm table-striped">
         <tbody>
             <tr>
                 <th>Order</th>
@@ -16,7 +16,17 @@
                 <td>{{ $orderHistory->created_at ? $orderHistory->created_at->format('d-M-y H:i') : '' }}</td>
             </tr>
         </tbody>
-    </table>
+    </table> --}}
     {{-- <span class="font-bold">Description:</span> --}}
-    <span class="text-gray-600">{{ $orderHistory->description }}</span>
+    {{-- <span class="text-gray-600 w-1/3">
+    {{ $orderHistory->created_at ? $orderHistory->created_at->format('d-M-y H:i') : '' }}
+    </span> --}}
+    <div class="w-1/3">
+        <i class="fas fa-circle-chevron-right text-sm mr-2"></i>
+        {{ $orderHistory->action_type }}
+    </div>
+
+    <div class=" w-2/3">
+        {{ $orderHistory->description }}
+    </div>
 </div>
--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -143,10 +143,14 @@
 
         {{-- {{ $order }} --}}
     </div>
-    <div class="flex flex-col">
-        @foreach ($histories as $history)
+    <div class="flex flex-col bg-gray-50 rounded-md shadow-sm p-2 mx-2 ">
+        <h4 class="text-lg font-bold mb-2 self-center text-center w-full bg-slate-100 rounded-md shadow-inner">Order History</h4>
+        <div class="h-min overflow-y-scroll max-h-[600px]">
+
+            @foreach ($histories as $history)
             @include('layouts.partials.orderhistory.show', ['orderHistory' => $history])
-        @endforeach
+            @endforeach
+        </div>
     </div>
 
 
